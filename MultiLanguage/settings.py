81974c6bab835ac6778fb9d1e3d249834c195dab
CAFreelance-13 <vijayjakhar.vj@gmail.com>
2020-04-18 22:36:39 +0530

    Static and media file added in settings and url

--- MultiLanguage/settings.py
+++ MultiLanguage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z8ka(%1@tr!(w(v4mb*3q6yksa%2k-8+hqia1j*5*w6ly%!64!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
